718436dfa646ceea300827d82d6b510963f6c228
zhanzhan <xyy250226@antfin.com>
2020-05-16 23:32:20 +0800

    testF

--- test.java
+++ test.java
@@ -6,7 +6,7 @@ public class Test {
 
     public static void main(String[] args) throws ParseException {
      
-        List<String> test = Arrays.asList("aa","bb","cc","testE update");
+        List<String> test = Arrays.asList("aa","bb","cc","dd");
 
     }
 
