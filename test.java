34a24a435df7f3ec8bd4b1d068bfdf94916b2efe
zhanzhan <xyy250226@antfin.com>
2020-05-16 16:23:51 +0800

    add a file

---
+++ test.java
@@ -0,0 +1,13 @@
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) throws ParseException {
+     
+        List<String> test = Arrays.asList("aa","bb","cc","dd");
+
+    }
+
+}
