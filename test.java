5dc6fe1221cc148271ef58ed1c76d8be2340610e
zhanzhan <xyy250226@antfin.com>
2020-05-16 16:27:04 +0800

    testA

--- test.java
+++ test.java
@@ -6,7 +6,7 @@ public class Test {
 
     public static void main(String[] args) throws ParseException {
      
-        List<String> test = Arrays.asList("aa","bb","cc","dd");
+        List<String> test = Arrays.asList("aa","bb","cc","testA change it ");
 
     }
 
